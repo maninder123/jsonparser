6fe127a117a9b82e1c6a283be38abbd2d1e7cc43
maninder123 <manindersingh221@gmail.com>
2024-10-18 16:59:34 +0530

    added mediaid to base64 code

---
+++ media_id to base64.py
@@ -0,0 +1,40 @@
+import base64
+from flask import Flask, request, jsonify
+import requests
+
+
+
+app = Flask(__name__)
+
+@app.route('/get_image', methods=['POST'])
+def get_image():
+    media_id = request.json.get('media_id')
+    headers = {
+            "Authorization": f"Bearer EAAPHZC1SC48ABO9LD4Gu4NZCO6lpWSwkgAWYEgPWXdT8wzCSaLokUlISD7hOgMxzZCXr8RXZCHCOdbUpNakCMQsWuDKiDIH9yZCZAIxKgcP65u3gOqOk4cjrvbdZBFNTBDT6fRweAO1YKEuwRUKLDzkvVcwShWyxcwPS6DMbBHuVzGO82BH7E3rRXkRZB0SnlEZA9kAZDZD",
+            # "User-Agent": self.user_agent
+        }
+    if not media_id:
+        return jsonify({'error': 'Media ID is required'}), 400
+
+    try:
+        # Call WhatsApp media retrieval URL to get the media URL
+        media_url_response = requests.get(f'https://graph.facebook.com/v19.0/{media_id}?phone_number_id=359029663964957',headers=headers)
+        media_url_data = media_url_response.json()
+        media_url = media_url_data.get('url', None)
+
+        if not media_url:
+            return jsonify({'error': 'Failed to retrieve media URL'}), 400
+
+        # Call WhatsApp media download API to get the image
+        image_response = requests.get(media_url,headers=headers)
+        image_data = image_response.content
+
+        # Convert image data to Base64
+        base64_image = base64.b64encode(image_data).decode('utf-8')
+
+        return jsonify({'base64_image': base64_image})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host="143.244.132.143", port=5009)
---
+++ url_to_base64.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import base64
+import requests
+
+app = Flask(__name__)
+
+def image_url_to_base64(image_url):
+    try:
+        # Send a GET request to the image URL
+        response = requests.get(image_url)
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Encode the image content to base64
+            base64_image = base64.b64encode(response.content).decode('utf-8')
+            return base64_image
+        else:
+            return None
+    except Exception as e:
+        return None
+
+@app.route('/convert-image', methods=['POST'])
+def convert_image():
+    data = request.json
+    if 'image_url' not in data:
+        return jsonify({"error": "Missing 'image_url' field"}), 400
+
+    image_url = data['image_url']
+    base64_image = image_url_to_base64(image_url)
+
+    if base64_image:
+        return jsonify({"base64_image": base64_image}), 200
+    else:
+        return jsonify({"error": "Failed to convert image"}), 400
+
+
+if __name__ == '__main__':
+    app.run(host="143.244.132.143", port=5008)
